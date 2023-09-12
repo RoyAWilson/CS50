cdcccbeea2f2c618c3a44c8e2d0ec78b6abac169
Roy Wilson <royawilson@hotmail.co.uk>
2023-09-12 18:40:52 +0100

    Func to print column of #s

---
+++ Code/4_loops/mario.py
@@ -0,0 +1,19 @@
+def main():
+    '''
+    
+    Print a column of #s
+    
+    '''
+    
+    print_columns(3)
+    
+def print_columns(height):
+    '''
+    
+    print the given number of columns
+    
+    '''
+    
+    print('#\n' * height, end = '')
+
+main()
