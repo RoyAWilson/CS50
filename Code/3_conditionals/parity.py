472a68bd4be7558f6c88750bdd854986dc9e31c1
Roy Wilson <royawilson@hotmail.co.uk>
2023-09-12 14:44:16 +0100

    Realign and remove uneeded code

--- Code/3_conditionals/parity.py
+++ Code/3_conditionals/parity.py
@@ -21,12 +21,6 @@ def iseven(n):
     calculate if x is even or odd
 
     '''
-    return True if x % 2 == 0 else False
-
-x = int(input('What\'s x? '))
-if iseven(x):
-    print(f'{x} is even')
-else:
-    print(f'{x} is odd')
+    return True if n % 2 == 0 else False
 
 main()
