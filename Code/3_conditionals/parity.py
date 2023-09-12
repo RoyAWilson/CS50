56b2c53370c5a437d36b79ac55de1c4c1e5371d5
Roy Wilson <royawilson@hotmail.co.uk>
2023-09-12 14:39:02 +0100

    Define main & function call to find if # odd/even

---
+++ Code/3_conditionals/parity.py
@@ -0,0 +1,28 @@
+'''
+    
+    Determine if number is odd or even
+    
+    '''
+
+def main():
+    '''
+    
+    Determine if number is odd or even
+    
+    '''
+
+def iseven(n):
+    '''
+
+    calculate if x is even or odd
+
+    '''
+    return True if x % 2 == 0 else False
+
+x = int(input('What\'s x? '))
+if iseven(x):
+    print(f'{x} is even')
+else:
+    print(f'{x} is odd')
+
+main()
