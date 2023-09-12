c78900af0d63e735429765fe11239e4fb8d2fd09
Roy Wilson <royawilson@hotmail.co.uk>
2023-09-12 15:13:57 +0100

    Houses using match

--- Code/3_conditionals/house.py
+++ Code/3_conditionals/house.py
@@ -6,9 +6,11 @@ using match
 
 name = input('What\'s your name? ').title()
 
-if name == 'Harry' or name == 'Hermione' or name == 'Ron':
-    print('Gryffindor')
-elif name == 'Draco':
-    print('Slytherin')
-else:
-    print('Who?')
+match name:
+    case 'Harry' | 'Hermione' | 'Ron':
+        print('Gryffindor')
+    case 'Draco':
+        print('Slytherin')
+    case _:
+        print('Who?')
+    
