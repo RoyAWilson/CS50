9e1e6a8ff71b77dcc9cb734108c4606d79282597
Roy Wilson <royawilson@hotmail.co.uk>
2023-09-12 14:41:28 +0100

    Change order & indent

--- Code/3_conditionals/parity.py
+++ Code/3_conditionals/parity.py
@@ -10,7 +10,11 @@ def main():
     Determine if number is odd or even
     
     '''
-
+    x = int(input('What\'s x? '))
+    if iseven(x):
+        print(f'{x} is even')
+    else:
+        print(f'{x} is odd')
 def iseven(n):
     '''
 
