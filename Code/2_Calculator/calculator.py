8ba6a875654ca11346c21e19c917e4ae54be5f98
Roy Wilson <royawilson@hotmail.co.uk>
2023-09-12 12:32:37 +0100

    Basic float addition with rounding 2 ways

--- Code/2_Calculator/calculator.py
+++ Code/2_Calculator/calculator.py
@@ -1,3 +1,6 @@
 x = float(input('What is x? '))
 y = float(input('What is y? '))
-print(f'{x} plus {y} = {round(x + y)}')
+print(f'{round(x)} plus {round(y)} = {round(x + y):,}')
+# Or:
+z = round(x + y)
+print(f'{z:,}')
