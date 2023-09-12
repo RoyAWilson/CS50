47353e48138edc6ffd962577e2cc95ee17223f68
Roy Wilson <royawilson@hotmail.co.uk>
2023-09-12 14:09:29 +0100

    Optimise if and elif statements

--- Code/3_conditionals/grade.py
+++ Code/3_conditionals/grade.py
@@ -5,15 +5,15 @@ To show the use of and keyword considering grades a student should get
 '''
 
 score = int(input('What is the score: '))
-if 90 <= score <= 100:
+if 90 >= 100:
     print('Grade: A')
-elif 80 <= score < 90:
+elif score > 80:
     print('Grade: B')
-elif 70 <= score < 80:
+elif score > 70:
     print('Grade: C')
-elif 60 <=score < 70:
+elif score > 60:
     print('Grade: C')
-elif 50 <= score < 60:
+elif score > 50:
     print('Grade: D')
 else:
     print('Grade: F')
