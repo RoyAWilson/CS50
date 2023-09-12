554dbe89574e856242557a291b42a2b9f6d6a604
Roy Wilson <royawilson@hotmail.co.uk>
2023-09-12 13:41:09 +0100

    rename file to correct spelling

--- Code/3_conditionals/compair.py
+++
@@ -1,9 +0,0 @@
-x = int(input('What\'s x? '))
-y = int(input('What\'s y? '))
-
-if x < y:
-    print(f"{x} is less than {y}")
-elif x > y:
-    print(f"{x} is greater than {y}")
-else:
-    print("x and y are equal")
