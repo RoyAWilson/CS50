1938ca8b8915bbd2e219e8a1b3f88fdcb99a07a7
Roy Wilson <royawilson@hotmail.co.uk>
2023-09-12 18:47:54 +0100

    print with func

---
+++ Code/4_loops/mario_2.py
@@ -0,0 +1,19 @@
+def main():
+    '''
+    
+    Print a horizontal n of ?s
+    
+    '''
+  
+    print_row(4)
+ 
+def print_row(width):
+    '''
+    
+    define the function to print the ?s
+    
+    '''
+    print('?' * width, end='')
+
+
+main()
