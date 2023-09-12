54742d0eb30f6b22e6088c6f09e0bf5f2941e17a
Roy Wilson <royawilson@hotmail.co.uk>
2023-09-12 15:02:23 +0100

    Houses if and elif

---
+++ Code/3_conditionals/house.py
@@ -0,0 +1,18 @@
+'''
+
+using match
+
+'''
+
+name = input('What\'s your name? ').title()
+
+if name == 'Harry':
+    print('Gryffindor')
+elif name == 'Hermione':
+    print('Gryffindor')
+elif name == 'Ron':
+    print('Gryffindor')
+elif name == 'Draco':
+    print('Slytherin')
+else:
+    print('Who?')
