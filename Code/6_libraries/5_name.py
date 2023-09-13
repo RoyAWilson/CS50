958070e1752c0520ae515531b0959e4a78fa3aaf
Roy Wilson <royawilson@hotmail.co.uk>
2023-09-13 13:05:30 +0100

    handle index error with if elif and sys.exit

---
+++ Code/6_libraries/5_name.py
@@ -0,0 +1,30 @@
+'''
+
+Grab input from command line
+
+'''
+import sys
+# try:
+#     print('Hello! My name is ', sys.argv[1])
+# except IndexError:
+#     print('Too few arguments passed')
+    
+# Another way to do it, more sensible but still not perfect:
+
+# if len(sys.argv) < 2:
+#     print('Too few arguments passed')
+# elif len(sys.argv) > 2:
+#     print('Too many arguments passed')
+# else:
+#     print('Hello, my name is ', sys.argv[1])
+
+# And another way of doing it:
+# if len(sys.argv) < 2:
+#     sys.exit('Too few arguments passed')
+# elif len(sys.argv) > 2:
+#     sys.exit('Too many arguments passed')
+
+# print('Hello, my name is ', sys.argv[1])
+
+
+# For multiple arguments:
