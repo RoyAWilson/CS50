2d0a552975bc7d0fcdf338702867d988b5612e13
Roy Wilson <royawilson@hotmail.co.uk>
2023-09-13 17:58:18 +0100

    Basic unit test code

--- Code/7_unit_tests/test_number.py
+++ Code/7_unit_tests/test_number.py
@@ -39,7 +39,22 @@ def test_square():
         assert(square(3)) == 9
     except AssertionError:
         print('3 squared was not 9')
+    try:
+        assert( square(-2)) == 4
+    except AssertionError:
+        print("-2 squared was not 4")
+    try:
+        assert(square(-3)) == 9
+    except AssertionError:
+        print('-3 squared was not 9')
+    try:
+        assert(square(0)) == 0
+    except AssertionError:
+        print('0 squared was not 0')            
+     
 # puttin in error handling.  To handle the assertion error and make the output more user friendly    
+
+
 if __name__ == '__main__':
     main()
     
