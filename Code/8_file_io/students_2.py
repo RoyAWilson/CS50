1eb3aae2d045c00ccc6c05ee0b26d7c8355e8efc
Roy Wilson <royawilson@hotmail.co.uk>
2023-09-14 14:42:31 +0100

    Make dictionary and sort by key on name field

---
+++ Code/8_file_io/students_2.py
@@ -0,0 +1,24 @@
+'''
+Simple read CSV file
+
+'''
+students = []
+student = {}
+with open('Students.csv','r', encoding='utf-8') as file:
+    for line in file:
+        name, house = line.rstrip().split(',')
+        student = {'name': name, 'house': house}
+        students.append(student)
+file.close()
+
+# Define func to get name of student for sorting by key:
+
+def get_name(student):
+    '''
+    func to get name for sorting purposes:
+    '''
+    return student['name']
+
+for student in sorted(students, key=get_name):
+    print(f'{student["name"]} is in house {student["house"]}')
+    
