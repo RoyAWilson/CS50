436f8e2d4d2ea7533583871e2e9b7c473b6a0f0e
Roy Wilson <royawilson@hotmail.co.uk>
2023-09-14 14:05:16 +0100

    Simple read csv file.

---
+++ Code/8_file_io/students.py
@@ -0,0 +1,11 @@
+'''
+Simple read CSV file
+
+'''
+
+with open('Students.csv','r', encoding='utf-8') as file:
+    for line in file:
+        row = line.rstrip().split(',')
+        print(f'{row[0]} is in {row[1]}')
+file.close()
+ 
