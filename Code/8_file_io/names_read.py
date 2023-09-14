5894d9c8fd353e8afbad47e8356292f7575e1d17
Roy Wilson <royawilson@hotmail.co.uk>
2023-09-14 13:20:36 +0100

    Read Names.txt and print out contents sorted.

--- Code/8_file_io/names_read.py
+++ Code/8_file_io/names_read.py
@@ -4,6 +4,10 @@ To read the names.txt file
 
 '''
 
+names = []
 with open('Name.txt', 'r') as file:
     for line in file:
-        print('Hello, ', line.rstrip())
+        names.append(line.rstrip())
+for name in sorted(names):
+    print(f'Hello, {name}')
+
