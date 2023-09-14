c3ea1443b44b65420fda8ad47135287c7663dc6e
Roy Wilson <royawilson@hotmail.co.uk>
2023-09-14 17:33:52 +0100

    Write to file using DictWriter from csv library

---
+++ Code/8_file_io/students_dictwriter.py
@@ -0,0 +1,15 @@
+'''
+
+Write to CSV file with DictWriter
+
+'''
+
+import csv
+
+name = input('Enter you name: ')
+home = input('Enter your home: ')
+
+with open('names_dictwriter.csv', 'a', newline = '') as file:
+    writer = csv.DictWriter(file, fieldnames=['name', 'home'])
+    writer.writerow({'name': name, 'home': home})
+file.close()
