6549fd10938b02548b66301a576426f83afdc522
Roy Wilson <royawilson@hotmail.co.uk>
2023-09-15 13:02:06 +0100

    Tidied up slightly

--- Code/8_file_io/names.py
+++ Code/8_file_io/names.py
@@ -5,6 +5,6 @@ Basics of file io to save a few names in a text file
 '''
 
 name = input('What\'s your name? ')
-with open('../../Data/Name.txt','a') as file:
+with open('../../Data/Name.txt','a', encoding='utf-8') as file:
     file.write(f'{name}\n')
 file.close()
