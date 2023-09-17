e631c367449ec9adbe104c00e61bb75b2a86e9df
Roy Wilson <royawilson@hotmail.co.uk>
2023-09-17 18:52:31 +0100

    add some descriptions and help a default and type to argparse

--- Code/11_etc/meows.py
+++ Code/11_etc/meows.py
@@ -2,16 +2,16 @@
 
 import argparse
 
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(description = 'Meow like a cat')
 
 # Configure argparser to recognise the argument we want to pass and parse
 
-parser.add_argument('-n')
+parser.add_argument('-n', default = 1, help = 'Number of times to meow', type = int)
 args = parser.parse_args()
 # argparser will automaticalls read sys.argsv for me.  No need to bother doing it for myself
 # args will now contain all the arguments passed at the command line and can be
 # iterated over using a loop:
 
 # start loop in a the range of
-for _ in range(int(args.n)):
+for _ in range(args.n):
     print('meow')
