00dec3c8bae00c415b4141dd473c639949b0df67
Roy Wilson <royawilson@hotmail.co.uk>
2023-09-17 16:32:40 +0100

    Implement account as a class

--- Code/11_etc/bank.py
+++ Code/11_etc/bank.py
@@ -1,27 +1,46 @@
 '''
-
-Global Variables intro
-
+to implement a bank account class
+and allow it to have a balance 
+and deposit and withdrawal methods
 '''
 
-# set global balance variable:
+class Account:
+    '''
+    set up a banking account
+    '''
+    def __init__(self,):
+        self._balance = 0
+        # _balance as visual reminder this variable should only be changed by this class
 
-balance = 0
+    @property
+    def balance(self):
+        '''
+        return the balance
+        '''
+        return self._balance
+
+    def deposit(self, n):
+        '''
+        Make a deposit
+        '''
+        self._balance += n
+
+    def withdraw(self, n):
+        '''
+        Make a withdrawal
+        '''
+        self._balance -= n
 
 def main():
-    print('Balance=', balance)
-    deposit(100)
-    withdraw(50)
-    print('New balance= ', balance)
-    
-def deposit(n):
-    global balance
-    balance += n
-
-def withdraw(n):
-    global balance
-    balance -= n
-
+    '''
+    Account transactions
+    '''
+    account = Account()
+    print(account.balance)
+    account.deposit(100)
+    account.withdraw(50)
+    print('Balance = ', account.balance)
 
 if __name__ == '__main__':
     main()
+        
