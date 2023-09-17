c4820ba1edd347eb9244c5b3d837de625cfa6af9
Roy Wilson <royawilson@hotmail.co.uk>
2023-09-17 17:36:11 +0100

    Fix error of none printing by changing code to return a string of n meows.

--- Code/11_etc/meows.py
+++ Code/11_etc/meows.py
@@ -2,9 +2,8 @@
 constants intro
 '''
 
-def meow(n: int) -> None:
-    for _ in range(n):
-        print('meow')
+def meow(n: int) -> str:
+    return 'meow\n' * n
 
 number: int = int(input('Number: '))
 meows: str = meow(number)
