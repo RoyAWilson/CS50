b224c0e4de42cd91d67691c53a19814089112f82
Roy Wilson <royawilson@hotmail.co.uk>
2023-09-17 17:51:11 +0100

    Meow with docstring added to function

--- Code/11_etc/meows.py
+++ Code/11_etc/meows.py
@@ -1,8 +1,13 @@
-'''
-constants intro
-'''
-
 def meow(n: int) -> str:
+    '''
+    to meow n times
+    
+    :param n: Number of tiems to meow
+    :type n: int
+    :raise TypeError: if n is not an int
+    :return: A string of n meows, one per line
+    :rtype: str
+    '''
     return 'meow\n' * n
 
 number: int = int(input('Number: '))
