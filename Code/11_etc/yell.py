ee68debd1a211f2f75db31ddb6740411f5c6e7dd
Roy Wilson <royawilson@hotmail.co.uk>
2023-09-17 21:17:47 +0100

    Yell with list comprehension

--- Code/11_etc/yell.py
+++ Code/11_etc/yell.py
@@ -5,10 +5,11 @@ def main():
 
 def yell(*words):
     # *words is just a rename of *args
-    uppercased = []
-    for word in words:
-        uppercased.append(word.upper())
+    uppercased = [word.upper() for word in words]
+    # Note the use of () on upper has returned.
     print(*uppercased)
+    # Note the * in the print statement to unpack the vaules.
+        
 ## Note the single *, this is saying print the unpacked list - it is passing 3 arguments to the print func.
 
 if __name__ == '__main__':
