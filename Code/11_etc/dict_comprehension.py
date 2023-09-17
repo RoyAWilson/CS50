de97838c045c8f37b01fc2cb1ef74f847f50794e
Roy Wilson <royawilson@hotmail.co.uk>
2023-09-17 22:12:41 +0100

    Dictionary comprehension

--- Code/11_etc/dict_comprehension.py
+++ Code/11_etc/dict_comprehension.py
@@ -1,6 +1,8 @@
 students = ['Hermione', 'Harry', 'Ron']
 
-gryffindors = [{'name': student, 'house': 'Gryffindor'} for student in students]
+gryffindors = {student: 'Gryffindor' for student in students}
+## can see this being useful with another dictionary of column headers in
+## writing a csv file or exporting to a database.
 
 print(gryffindors)
     
