2b26f057e5fdd364d7bc0835f4e7c8a2106f69a4
Roy Wilson <royawilson@hotmail.co.uk>
2023-09-17 20:52:28 +0100

    Yell with unpacked list printed to terminal

--- Code/11_etc/yell.py
+++ Code/11_etc/yell.py
@@ -1,11 +1,14 @@
 # to take an input and yell it back in all caps
 
 def main():
-    yell('This is CS50')
+    yell(['This', 'is', 'CS50'])
 
-def yell(phrase):
-    print(phrase.upper())
-
+def yell(words):
+    uppercased = []
+    for word in words:
+        uppercased.append(word.upper())
+    print(*uppercased)
+## Note the single *, this is saying print the unpacked list - it is passing 3 arguments to the print func.
 
 if __name__ == '__main__':
     main()
